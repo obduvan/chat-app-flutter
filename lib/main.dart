aaa71c6b20f5c885acb3466f9918f7ce220aaf5f
obduvan <obduvann@gmail.com>
2021-08-23 02:02:12 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// data
+// domain
+// internal (add dependecies)
+// ui
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Messio',
+        theme: ThemeData(primarySwatch: Colors.yellow),
+        home: MyHomePage(title: 'MyChat'));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Hi world!"),
+      ),
+    );
+  }
+}

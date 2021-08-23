7c34218db60105a0fad8dd2823256d4cca8b467f
obduvan <obduvann@gmail.com>
2021-08-24 01:53:36 +0500

    added first pages

---
+++ lib/config_style/color_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette{
+  static const primaryTextColor = Colors.blue;
+  static const secondaryTextColor = Colors.blue;
+  static const primaryBackgroundColor = Colors.blue;
+  static const secondaryColor = Colors.black;
+}
---
+++ lib/presentation/pages/ConversationPage.dart
@@ -0,0 +1,26 @@
+// import 'package:flutter/material.dart';
+
+// class ConversationPage extends StatefulWidget {
+//   ConversationPage({Key? key}) : super(key: key);
+
+//   @override
+//   ConversationPageState createState() => ConversationPageState();
+// }
+
+// class ConversationPageState extends State<ConversationPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: ChatAppBar(),
+//         body: Stack(
+//           children: <Widget>[
+//             Column(
+//               children: <Widget>[
+//                 ChatListWidget(),
+//                  InputWidget()
+//                  ],
+//             )
+//           ],
+//         ));
+//   }
+// }
